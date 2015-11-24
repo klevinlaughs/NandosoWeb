004b75a44c93a425a335196082a49447b7b0afdd
Kelvin <kclau95@gmail.com>
2015-11-24 22:25:17 +1300

    Initial files

---
+++ NandosoWeb/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NandosoWeb.Models
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public DateTime date { get; set; }
+        public string message { get; set; }
+    }
+}
---
+++ NandosoWeb/Models/Special.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NandosoWeb.Models
+{
+    public enum type
+    {
+        Weight, Item
+    }
+
+    public class Special
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public double originalPrice { get; set; }
+        public double discount { get; set; }
+        public type type { get; set; }
+    }
+}

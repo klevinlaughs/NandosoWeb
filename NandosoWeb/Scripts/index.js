7d8f963611a5eb89b9c765dcb5078479f7294778
Kelvin <kclau95@gmail.com>
2015-11-24 23:07:39 +1300

    Created index.js

---
+++ NandosoWeb/Scripts/index.js
@@ -0,0 +1,9 @@
+﻿// Run this when the document is loaded
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Page loaded...");
+    getComments();
+});
+
+function getComments() {
+    console.log("Getting comments...");
+}

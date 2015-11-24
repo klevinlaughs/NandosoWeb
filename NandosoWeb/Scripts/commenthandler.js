f7f1eb730dcfb69f04ff9edd43b58462c42df3bb
Kelvin <kclau95@gmail.com>
2015-11-25 03:03:51 +1300

    Added postComment() function in commenthandler.js

--- NandosoWeb/Scripts/commenthandler.js
+++ NandosoWeb/Scripts/commenthandler.js
@@ -19,9 +19,21 @@
 })();
 
 function postComment() {
+    var username = document.getElementById("username");
+    var email = document.getElementById("email");
+    var message = document.getElementById("message");
+
+    console.log(username.value);
+    console.log(email.value);
+    console.log(message.value);
     $.ajax({
         type: "POST",
         dataType: "json",
+        data:{
+            name: username.value,
+            email: email.value,
+            message: message.value
+        },
         url: "https://Nandosoklau158.azurewebsites.net/api/comments"
     });
 }

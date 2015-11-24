4dc7c41a2a8fd72d9eeb265c4b7430321a0ce3d4
Kelvin <kclau95@gmail.com>
2015-11-25 03:07:53 +1300

    Configured to use MySql on azure (not tested yet)

--- NandosoWeb/Models/NandosoWebContext.cs
+++ NandosoWeb/Models/NandosoWebContext.cs
@@ -7,8 +7,26 @@ using System.Web;
 
 namespace NandosoWeb.Models
 {
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
     public class NandosoWebContext : DbContext
     {
+
+        
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+        public NandosoWebContext() : base("name=NandosoWebContext")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<NandosoWebContext, MyConfiguration>());
+        }
+
+        public System.Data.Entity.DbSet<NandosoWeb.Models.Special> Specials { get; set; }
+
+        public System.Data.Entity.DbSet<NandosoWeb.Models.Comment> Comments { get; set; }
 
         public class MyConfiguration : DbMigrationsConfiguration<NandosoWebContext>
         {
@@ -59,23 +77,6 @@ namespace NandosoWeb.Models
 
             }
         }
-        // You can add custom code to this file. Changes will not be overwritten.
-        // 
-        // If you want Entity Framework to drop and regenerate your database
-        // automatically whenever you change your model schema, please use data migrations.
-        // For more information refer to the documentation:
-        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
-
-        public NandosoWebContext() : base("name=NandosoWebContext")
-        {
-            Database.SetInitializer(new MigrateDatabaseToLatestVersion<NandosoWebContext, MyConfiguration>());
-        }
-
-        public System.Data.Entity.DbSet<NandosoWeb.Models.Special> Specials { get; set; }
-
-        public System.Data.Entity.DbSet<NandosoWeb.Models.Comment> Comments { get; set; }
-
-
 
     }
 }
